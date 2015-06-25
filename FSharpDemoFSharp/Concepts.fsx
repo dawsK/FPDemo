7cde7dc48f7ef0df5f21064205a248e6957d68a1
dawsK <dkroeker@gmail.com>
2015-06-24 23:01:53 -0500

    Add all source code for presentation

---
+++ FSharpDemoFSharp/Concepts.fsx
@@ -0,0 +1,48 @@
+﻿
+(** Currying **)
+
+// function with one parameter
+let printParameter x =
+    printfn "x=%i" x
+
+let printTwoParameters x  =      // only one parameter
+    let subFunction y =          // new inner function with one param
+        printfn "x=%i y=%i" x y  
+    subFunction                  // return the inner function
+
+// curried function that takes 2 parameters (sort of)
+let printTwoParametersCurried x y =
+    printfn "x=%i y=%i" x y
+
+printTwoParameters 1
+printTwoParametersCurried 1
+
+(** Partial Application **)
+
+let add x y = x + y
+let addOne = add 1
+let twoPlusOne = addOne 2
+
+// break it down:
+let add x y = x + y
+
+let add2 x =
+    let addInner y =
+        x + y
+    addInner
+// val add2 : x:int -> (int -> int)
+
+let addOne = add 1
+
+(** Recursion **)
+
+let sum values =
+    let rec calcSum total remainingValues = 
+        match remainingValues with
+        | [] -> total
+        | x :: xs -> calcSum (total + x) xs
+    calcSum 0 values
+
+sum [1;2;3;4;5]
+
+List.sum [1;2;3;4;5]
---
+++ FSharpDemoFSharp/WhyUseIt.fsx
@@ -0,0 +1,34 @@
+﻿
+// one-liners
+[1..100] |> List.sum |> printfn "sum=%d"
+
+// no curly braces, semicolons or parentheses
+let square x = x * x
+let sq = square 42
+
+// simple types in one line
+type Person = {Name : string; Age : int}
+
+// complex types in a few lines
+type Employee = 
+  | Worker of Person
+  | Manager of Employee list
+
+// type inference
+let jdoe = {Name="John Doe"; Age = 35}
+let worker = Worker jdoe
+
+// Concurrency
+
+let doSomething i = 
+    printfn "Doing something #%i..." i
+    System.Threading.Thread.Sleep(1000)
+    printfn "Done #%i" i
+
+let task = async { doSomething 1 }
+task |> Async.RunSynchronously
+
+let tasks = 
+    [ for i in 1 .. 40 -> async { doSomething i } ]
+    |> Async.Parallel
+    |> Async.RunSynchronously
